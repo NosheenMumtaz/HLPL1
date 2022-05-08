bc7ef637b3fab7e9e548bde001bb76e3c77941bd
NosheenMumtaz <99494225+NosheenMumtaz@users.noreply.github.com>
2022-05-08 12:26:11 +0200

    Update drill6.cpp

--- drill6.cpp
+++ drill6.cpp
@@ -1,8 +1,7 @@
-//PART NO 1:
+                                                            //CHAPTER NO 18
+                                                              //PART NO 1:
 #include "std_lib_facilities.h"
-
 int ga[10] ={1,2,4,8,16,32,64,128,256,512};   //defining global int array ga 
-
 void f(int a[], int n)
 {
 	
@@ -13,8 +12,6 @@ void f(int a[], int n)
 	
 	for (int i = 0;i < 10;i++)  //printing values of la
 		cout<<la[i]<<" ";
-
-	
 	int* p = new int[n];     // defining pointer p to int 
 	
 	for (int i = 0;i < 10;i++)   // copy values from argumented array into free store array
@@ -31,20 +28,17 @@ int main()
 	f(ga, 10);   // calling f() with ga as its argument
 	int aa[10] = {1,2,6,24,120,720,5040,40320,362880,3628800};  // defining array aa with ten elements
 	f(aa, 10); //calling f() with aa as its argument
-
 	
 	return 0;
 }
 
 
-//PART NO 2
+                                                                       //PART NO 2
+
 #include "std_lib_facilities.h"
-
 vector<int> gv ={1,2,4,8,16,32,64,128,256,512}; // defining global vector<int> gv
-
 void f(vector <int> v)
 {
-	
 	vector <int> lv;   // define local vector<int> lv 
 	
 	for (int i = 0;i < v.size();i++) //copying values from gv to lv
@@ -68,6 +62,5 @@ int main()
 	vector <int> vv = {1,2,6,24,120,720,5040,40320,362880,3628800}; //defining vector<int>vv with first ten fractional values
 	f(vv);   // calling f() with vv
 
-	
 	return 0;
 }
